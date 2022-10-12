1fa6c9716f0e324f4350d941db6713c78a5314ef
DaniellyBenicio <115299113+DaniellyBenicio@users.noreply.github.com>
2022-10-12 17:54:40 -0300

    Questão 07 da lista de prática
    Fiz colocando o espaçamento entre cada # e depois com o espaçamento por isso estou enviado dois arquivos.

---
+++ 7 questão.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL, "Portuguese_Brazil");
+	
+	printf("# # # # # # # # # # # # # # # # # # # #\n\n");	
+	
+	printf("#				      #\n\n");	
+	
+	printf("#				      #\n\n");	
+	
+	
+	printf("#            SEJA BEM-VINDO           #\n\n");	
+	
+	
+	printf("#				      #\n\n");	
+	
+	
+	printf("#				      #\n\n");	
+	
+	
+	printf("# # # # # # # # # # # # # # # # # # # #\n\n");	
+	
+	
+	
+	
+
+
+
+return 0;
+}
